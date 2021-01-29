f779fdb4073d40d1547979a4924465e4a8a18578
Ekaterina <catherine1147@yandex.ru>
2021-01-29 19:30:17 +0300

    camelcase

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,10 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] = arr[i].slice(0, 1).toUpperCase() + arr[i].slice(1);
+  }
+
+  return arr.join('');
 }
