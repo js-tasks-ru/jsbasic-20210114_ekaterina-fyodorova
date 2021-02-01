a268a4390f9135809a0de31234161d9a18f0bd7d
Ekaterina <catherine1147@yandex.ru>
2021-02-01 13:11:51 +0300

    task-5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,14 +4,17 @@
  * @returns {{min:number, max:number}}  объект
  */
 
-let str = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
 function getMinMax(str) {
-  let stringWithoutSpaces = str.split(' ').join();
-  console.log(stringWithoutSpaces);
-  let itemsDividedByComma = stringWithoutSpaces.split(',');
-  let numbersOnly = itemsDividedByComma
-    .filter((item) => item !== '' && isFinite(item));
-  console.log(numbersOnly)
+  let result = {};
 
+  let string = str.split(' ').join();
+  let newString = string.split(',');
+  let numbers = newString.filter((item) => item && isFinite(item));
+
+  numbers.sort((a, b) => a - b);
+
+  result.min = +numbers[0];
+  result.max = +numbers[numbers.length - 1];
+  return result;
 }
 
